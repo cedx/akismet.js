424ecec802f89cc79d500b63e38229ac70da9565
Cédric Belin <cedric@belin.io>
2013-12-19 12:22:50 +0100

    Added classes describing a comment and its author

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Package entry point.
+ * @module index
+ */
+module.exports={
+  Author: require('./lib/author'),
+  Comment: require('./lib/comment').Comment,
+  CommentType: require('./lib/comment').CommentType
+};
