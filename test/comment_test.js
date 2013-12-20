5bc73c6b0eb98acc8b6f13d788e1bfdabe700906
Cédric Belin <cedric@belin.io>
2013-12-20 16:56:43 +0100

    Code modularization: merged unit tests of author and comment modules

--- test/comment_test.js
+++ test/comment_test.js
@@ -6,14 +6,50 @@
 // Module dependencies.
 var assert=require('assert');
 var akismet=require('../index');
+
 var Author=akismet.Author;
 var Comment=akismet.Comment;
 var CommentType=akismet.CommentType;
 
+// Tests the features of `Author` class.
+describe('Author', function() {
+
+  // Tests the `Author.fromJSON()` method.
+  describe('.fromJSON()', function() {
+   it('should return a null reference with a non-object JSON string', function() {
+     assert.strictEqual(Author.fromJSON('foo'), null);
+   });
+
+   it('should return an empty instance with an empty JSON object', function() {
+     var author=Author.fromJSON({});
+     assert.strictEqual(author.email, null);
+     assert.strictEqual(author.url, null);
+   });
+
+   it('should return an initialized instance with a non-empty JSON object', function() {
+     var author=Author.fromJSON({ comment_author_email: 'cedric@belin.io', comment_author_url: 'http://belin.io' });
+     assert.equal(author.email, 'cedric@belin.io');
+     assert.equal(author.url, 'http://belin.io');
+   });
+  });
+
+  // Tests the `Author#toJSON()` method.
+  describe('#toJSON()', function() {
+   it('should return an empty JSON object with a newly created instance', function() {
+     assert.equal(new Author().toJSON(), '{}');
+   });
+
+   it('should return a non-empty JSON object with a initialized instance', function() {
+     var author=new Author({ name: 'Cédric Belin', email: 'cedric@belin.io', ipAddress: '127.0.0.1', url: 'http://belin.io' });
+     assert.equal(author.toJSON(), '{"comment_author":"Cédric Belin","comment_author_email":"cedric@belin.io","comment_author_url":"http://belin.io","user_ip":"127.0.0.1"}');
+   });
+  });
+});
+
 // Tests the features of `Comment` class.
 describe('Comment', function() {
 
-  // Tests the `Comment.fromJSON` method.
+  // Tests the `Comment.fromJSON()` method.
   describe('.fromJSON()', function() {
     it('should return a null reference with a non-object JSON string', function() {
       assert.strictEqual(Comment.fromJSON('foo'), null);
@@ -37,7 +73,7 @@ describe('Comment', function() {
     });
   });
 
-  // Tests the `Comment#toJSON` method.
+  // Tests the `Comment#toJSON()` method.
   describe('#toJSON()', function() {
     it('should return an empty JSON object with a newly created instance', function() {
       assert.equal(new Comment().toJSON(), '{}');
