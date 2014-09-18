93ca04c7daad0ffea313b6abf5f92979770aded0
Cédric Belin <cedric@belin.io>
2014-09-18 03:27:30 +0200

    Code factorization

--- bin/cli.js
+++ bin/cli.js
@@ -33,7 +33,6 @@ var Application={
     process.chdir(__dirname+'/..');
     process.title=this.name+'.js';
 
-    // Parse command line arguments.
     program._name=this.name;
     program
       .version(require('../package.json').version)
@@ -43,12 +42,33 @@ var Application={
       .option('--silent', 'silence the log output from the server')
       .parse(process.argv);
 
-    // Start the server.
+    this._startServer(program.port, program.host, program.redirect ? program.redirect : null);
+  },
+
+  /**
+   * Prints the specified message, with a timestamp and a new line, to the standard output.
+   * @method _log
+   * @param {String|Function} message The message to be logged. If it's a function, the message is the result of the function call.
+   * @private
+   */
+  _log: function(message) {
+    if(!program.silent) console.log('[%s] %s', new Date().toUTCString(), message instanceof Function ? message() : message);
+  },
+
+  /**
+   * Starts a server listening for HTTP requests.
+   * @method _startServer
+   * @param {Number} port The port that the server should run on.
+   * @param {String} host The host that the server should run on.
+   * @param {Uri} [redirectUrl] The URL to redirect the user when a request is unhandled.
+   * @private
+   */
+  _startServer: function(port, host, redirectUrl) {
     var self=this;
-    var server=new Server({ redirectUrl: program.redirect ? program.redirect : null });
+    var server=new Server({ redirectUrl: redirectUrl });
 
     server.on('error', function(err) {
-      self.log(util.format('ERROR - %s', err));
+      self._log(util.format('ERROR - %s', err));
     });
 
     server.on('request', function(req) {
@@ -62,19 +82,9 @@ var Application={
       ));
     });
 
-    server.listen(program.port, program.host, function() {
-      self._log(util.format('Akismet server listening on %s:%d', program.host, program.port));
+    server.listen(port, host, function() {
+      self._log(util.format('Akismet server listening on %s:%d', host, port));
     });
-  },
-
-  /**
-   * Prints the specified message, with a timestamp and a new line, to the standard output.
-   * @method _log
-   * @param {String|Function} message The message to be logged. If it's a function, the message is the result of the function call.
-   * @private
-   */
-  _log: function(message) {
-    if(!program.silent) console.log('[%s] %s', new Date().toUTCString(), message instanceof Function ? message() : message);
   }
 };
 
