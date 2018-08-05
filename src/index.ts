f1869dab680d1087ee105afee03c53603f2a2bc9
Cédric Belin <cedric@belin.io>
2018-08-05 22:53:10 +0200

    Starting the TypeScript port

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const {Author} = require('./author.js');
+const {Blog} = require('./blog.js');
+const {Client, ClientError} = require('./client.js');
+const {Comment, CommentType} = require('./comment.js');
+
+module.exports = {
+  Author,
+  Blog,
+  Client,
+  ClientError,
+  Comment,
+  CommentType
+};
--- test/client_test.ts
+++ test/client_test.ts
@@ -1,5 +1,3 @@
-'use strict';
-
 const {expect} = require('chai');
 const {Author, Client, Comment, CommentType} = require('../lib/index.js');
 
