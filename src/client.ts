9a0ae3f1fcfc9e29f1530b9b26378f334d61f1c3
Cédric Belin <cedric@belin.io>
2018-08-26 14:07:38 +0200

    Changed the signature of the `Client` constructor

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,10 @@
 import {EventEmitter} from 'events';
-import * as fetch from 'node-fetch';
+import fetch, {Request, Response} from 'node-fetch';
 
 // @ts-ignore: disable processing of the imported JSON file.
 import * as pkg from '../package.json';
 import {Blog} from './blog';
+import {JsonMap} from './map';
 
 /**
  * Submits comments to the [Akismet](https://akismet.com) service.
@@ -11,50 +12,56 @@ import {Blog} from './blog';
 export class Client extends EventEmitter {
 
   /**
-   * Creates a new client.
-   * @param {string} apiKey The Akismet API key.
-   * @param {Blog|string} blog The front page or home URL of the instance making requests.
-   * @param {Object} [options] An object specifying values used to initialize this instance.
+   * An event that is triggered when a request is made to the remote service.
+   * @event request
    */
-  constructor(apiKey, blog, {endPoint = 'https://rest.akismet.com', isTest = false, userAgent = ''} = {}) {
+  static readonly eventRequest: string = 'request';
+
+  /**
+   * An event that is triggered when a response is received from the remote service.
+   * @event response
+   */
+  static readonly eventResponse: string = 'response';
+
+  /**
+   * The URL of the API end point.
+   */
+  endPoint: URL;
+
+  /**
+   * Value indicating whether the client operates in test mode.
+   * You can use it when submitting test queries to Akismet.
+   */
+  isTest: boolean;
+
+  /**
+   * The user agent string to use when making requests.
+   * If possible, the user agent string should always have the following format: `Application Name/Version | Plugin Name/Version`.
+   */
+  userAgent: string;
+
+  /**
+   * Creates a new client.
+   * @param apiKey The Akismet API key.
+   * @param blog The front page or home URL of the instance making requests.
+   * @param options An object specifying values used to initialize this instance.
+   */
+  constructor(public apiKey: string, public blog: Blog, options: Partial<ClientOptions> = {}) {
     super();
 
-    /**
-     * The Akismet API key.
-     * @type {string}
-     */
-    this.apiKey = apiKey;
+    const {
+      endPoint = new URL('https://rest.akismet.com'),
+      isTest = false,
+      userAgent = `Node.js/${process.version.substr(1)} | Akismet/${pkg.version}`
+    } = options;
 
-    /**
-     * The front page or home URL of the instance making requests.
-     * @type {Blog}
-     */
-    this.blog = typeof blog == 'string' ? new Blog(blog) : blog;
-
-    /**
-     * The URL of the API end point.
-     * @type {URL}
-     */
-    this.endPoint = typeof endPoint == 'string' ? new URL(endPoint) : endPoint;
-
-    /**
-     * Value indicating whether the client operates in test mode.
-     * You can use it when submitting test queries to Akismet.
-     * @type {boolean}
-     */
+    this.endPoint = endPoint;
     this.isTest = isTest;
-
-    /**
-     * The user agent string to use when making requests.
-     * If possible, the user agent string should always have the following format: `Application Name/Version | Plugin Name/Version`.
-     * @type {string}
-     */
-    this.userAgent = userAgent.length ? userAgent : `Node.js/${process.version.substr(1)} | Akismet/${pkg.version}`;
+    this.userAgent = userAgent;
   }
 
   /**
    * The class name.
-   * @type {string}
    */
   get [Symbol.toStringTag](): string {
     return 'Client';
@@ -62,40 +69,40 @@ export class Client extends EventEmitter {
 
   /**
    * Checks the specified comment against the service database, and returns a value indicating whether it is spam.
-   * @param {Comment} comment The comment to be checked.
-   * @return {Promise<boolean>} A boolean value indicating whether it is spam.
+   * @param comment The comment to be checked.
+   * @return A boolean value indicating whether it is spam.
    */
-  async checkComment(comment) {
-    const endPoint = new URL('/1.1/comment-check', `${this.endPoint.protocol}//${this.apiKey}.${this.endPoint.host}`);
+  async checkComment(comment: Comment): Promise<boolean> {
+    const endPoint = new URL('1.1/comment-check', `${this.endPoint.protocol}//${this.apiKey}.${this.endPoint.host}`);
     return await this._fetch(endPoint, comment.toJSON()) == 'true';
   }
 
   /**
    * Submits the specified comment that was incorrectly marked as spam but should not have been.
-   * @param {Comment} comment The comment to be submitted.
-   * @return {Promise} Completes once the comment has been submitted.
+   * @param comment The comment to be submitted.
+   * @return Completes once the comment has been submitted.
    */
-  async submitHam(comment) {
-    const endPoint = new URL('/1.1/submit-ham', `${this.endPoint.protocol}//${this.apiKey}.${this.endPoint.host}`);
+  async submitHam(comment: Comment): Promise<void> {
+    const endPoint = new URL('1.1/submit-ham', `${this.endPoint.protocol}//${this.apiKey}.${this.endPoint.host}`);
     return this._fetch(endPoint, comment.toJSON());
   }
 
   /**
    * Submits the specified comment that was not marked as spam but should have been.
-   * @param {Comment} comment The comment to be submitted.
-   * @return {Promise} Completes once the comment has been submitted.
+   * @param comment The comment to be submitted.
+   * @return Completes once the comment has been submitted.
    */
-  async submitSpam(comment) {
-    const endPoint = new URL('/1.1/submit-spam', `${this.endPoint.protocol}//${this.apiKey}.${this.endPoint.host}`);
+  async submitSpam(comment: Comment): Promise<void> {
+    const endPoint = new URL('1.1/submit-spam', `${this.endPoint.protocol}//${this.apiKey}.${this.endPoint.host}`);
     return this._fetch(endPoint, comment.toJSON());
   }
 
   /**
    * Checks the API key against the service database, and returns a value indicating whether it is valid.
-   * @return {Promise<boolean>} A boolean value indicating whether it is a valid API key.
+   * @return A boolean value indicating whether it is a valid API key.
    */
-  async verifyKey() {
-    const endPoint = new URL('/1.1/verify-key', this.endPoint);
+  async verifyKey(): Promise<boolean> {
+    const endPoint = new URL('1.1/verify-key', this.endPoint);
     return await this._fetch(endPoint, {key: this.apiKey}) == 'valid';
   }
 
@@ -107,26 +114,26 @@ export class Client extends EventEmitter {
    * @event {Request} The "request" event.
    * @event {Response} The "response" event.
    */
-  async _fetch(endPoint, fields) {
+  async _fetch(endPoint: URL, fields: JsonMap) {
     const body = new URLSearchParams(Object.assign(this.blog.toJSON(), fields));
     if (this.isTest) body.set('is_test', '1');
 
-    const req = new fetch.Request(endPoint.href, {
-      method: 'POST',
+    const req = new Request(endPoint.href, {
+      body,
       headers: {'content-type': 'application/x-www-form-urlencoded', 'user-agent': this.userAgent},
-      body
+      method: 'POST'
     });
 
-    this.emit('request', req);
+    this.emit(Client.eventRequest, req);
 
-    let res;
+    let res: Response;
     try { res = await fetch(req); }
     catch (err) { throw new ClientError(err.message, endPoint); }
 
-    this.emit('response', req, res);
+    this.emit(Client.eventResponse, req, res);
 
     if (!res.ok) throw new ClientError('An error occurred while querying the end point', endPoint);
-    if (res.headers.has('x-akismet-debug-help')) throw new ClientError(res.headers.get('x-akismet-debug-help'), endPoint);
+    if (res.headers.has('x-akismet-debug-help')) throw new ClientError(res.headers.get('x-akismet-debug-help')!, endPoint);
     return res.text();
   }
 }
@@ -138,23 +145,12 @@ export class ClientError extends Error {
 
   /**
    * Creates a new client error.
-   * @param {string} message A message describing the error.
-   * @param {string|URL} [uri] The URL of the HTTP request or response that failed.
+   * @param message A message describing the error.
+   * @param uri The URL of the HTTP request or response that failed.
    */
-  constructor(message, uri) {
+  constructor(message: string = '', readonly uri: URL | null = null) {
     super(message);
-
-    /**
-     * The error name.
-     * @type {string}
-     */
     this.name = 'ClientError';
-
-    /**
-     * The URL of the HTTP request or response that failed.
-     * @type {URL}
-     */
-    this.uri = typeof uri == 'string' ? new URL(uri) : uri;
   }
 
   /**
@@ -162,8 +158,29 @@ export class ClientError extends Error {
    * @return The string representation of this object.
    */
   toString(): string {
-    const values = `"${this.message}"`;
+    let values = `"${this.message}"`;
     if (this.uri) values = `${values}, uri: "${this.uri.href}"`;
     return `${this.name}(${values})`;
   }
+}
+
+/**
+ * Defines the options of a `Client` instance.
+ */
+export interface ClientOptions {
+
+  /**
+   * The URL of the API end point.
+   */
+  endPoint: URL;
+
+  /**
+   * Value indicating whether the client operates in test mode.
+   */
+  isTest: boolean;
+
+  /**
+   * The user agent string to use when making requests.
+   */
+  userAgent: string;
 }
