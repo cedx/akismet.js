ca14f96722020b8460080a71d851339c34de3e5e
Cédric Belin <cedric@belin.io>
2019-01-24 01:07:15 +0100

    Fix a packaging issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
+const commonjs = require('rollup-plugin-commonjs');
 const resolve = require('rollup-plugin-node-resolve');
+
 module.exports = {
   input: 'lib/browser.mjs',
   output: {file: 'build/akismet.js', format: 'iife', name: 'akismet'},
-  plugins: [resolve()]
+  plugins: [resolve(), commonjs({
+    namedExports: {'node_modules/eventemitter3/index.js': ['EventEmitter']}
+  })]
 };
