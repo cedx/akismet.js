fc662fbaad4b305f4d44075441d0a6a7c6332c8e
Cédric Belin <cedric@belin.io>
2019-06-01 19:22:52 +0200

    Ported the configuration files to JSON

--- etc/rollup.js
+++ etc/rollup.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
-  input: join(__dirname, '../lib/browser.mjs'),
+  input: join(__dirname, '../lib/browser.js'),
   output: {file: join(__dirname, '../build/akismet.js'), format: 'iife', name: 'akismet'},
   plugins: [resolve(), commonjs({
     namedExports: {'node_modules/eventemitter3/index.js': ['EventEmitter']}
