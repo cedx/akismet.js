50257fc662e29fa946182af9335cdf7b3b853db7
Cédric Belin <cedric@belin.io>
2019-06-09 19:41:40 +0200

    Updated the browser tests

--- etc/karma.js
+++ etc/karma.js
@@ -2,11 +2,18 @@ const {normalize, resolve} = require('path');
 const commonjs = require('rollup-plugin-commonjs');
 const nodeResolve = require('rollup-plugin-node-resolve');
 
+/** Resolves browser imports. */
+function browserResolve() {
+  return {
+    name: 'browserResolve',
+    resolveId: source => source == '../lib/index.js' ? resolve(__dirname, '../lib/browser.js') : null
+  };
+}
+
 module.exports = config => config.set({
   basePath: resolve(__dirname, '..'),
   browsers: ['FirefoxHeadless'],
   files: [
-    {pattern: 'lib/**/*.js', type: 'module'},
     {pattern: 'test/**/*.js', type: 'module'}
   ],
   frameworks: ['mocha'],
@@ -20,7 +27,7 @@ module.exports = config => config.set({
       warn(warning);
     },
     output: {format: 'iife', name: 'akismet'},
-    plugins: [nodeResolve(), commonjs()]
+    plugins: [browserResolve(), nodeResolve(), commonjs()]
   },
   singleRun: true
 });
