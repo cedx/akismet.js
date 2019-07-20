606bd20d098ed3ee6673542fb7cb1a429df6aae3
Cédric Belin <cedric@belin.io>
2019-07-21 00:34:45 +0200

    Updated the Karma settings

--- etc/karma.js
+++ etc/karma.js
@@ -1,36 +1,13 @@
-const {normalize, resolve} = require('path');
-const commonjs = require('rollup-plugin-commonjs');
-const nodeResolve = require('rollup-plugin-node-resolve');
-
-/**
- * Resolves browser imports.
- * @return {object} A Rollup plugin resolving browser imports.
- */
-function browserResolve() {
-  return {
-    name: 'browserResolve',
-    resolveId: source => source == '../lib/index.js' ? resolve(__dirname, '../lib/browser.js') : null
-  };
-}
+const {resolve} = require('path');
 
 module.exports = config => config.set({
   basePath: resolve(__dirname, '..'),
   browsers: ['FirefoxHeadless'],
   files: [
+    {pattern: 'lib/**/*.js', type: 'module'},
     {pattern: 'test/**/*.js', type: 'module'}
   ],
-  frameworks: ['mocha'],
-  preprocessors: {
-    'test/**/*.js': ['rollup']
-  },
+  frameworks: ['mocha', 'chai'],
   reporters: ['progress'],
-  rollupPreprocessor: {
-    onwarn: (warning, warn) => {
-      if (warning.code == 'CIRCULAR_DEPENDENCY' && warning.importer.includes(normalize('node_modules/chai'))) return;
-      warn(warning);
-    },
-    output: {format: 'iife', name: 'akismet'},
-    plugins: [browserResolve(), nodeResolve(), commonjs()]
-  },
   singleRun: true
 });
