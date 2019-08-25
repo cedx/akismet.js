c4fc760f1c6d6ea9504469b2b005cb20f4c7eb34
Cédric Belin <cedric@belin.io>
2019-08-25 20:54:31 +0200

    Updated the documentation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 if (typeof Reflect.get(global, 'fetch') != 'function') {
   // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-  // @ts-ignore: `fetch` has the wrong typings.
+  // @ts-ignore: `fetch` has wrong typings.
   const {Headers, Request, Response} = fetch;
   Object.assign(global, {fetch, Headers, Request, Response});
 }
