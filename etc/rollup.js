167924fbcc48c2d2253c56f5d612bc54b2e97cdc
Cédric Belin <cedric@belin.io>
2019-09-28 11:56:02 +0200

    Removed the usage of the `__dirname` value

--- etc/rollup.js
+++ etc/rollup.js
@@ -1,9 +1,7 @@
-import {resolve} from 'path';
-
 export default {
-  input: resolve(__dirname, '../lib/browser.js'),
+  input: 'lib/browser.js',
   output: {
-    file: resolve(__dirname, '../build/akismet.js'),
+    file: 'build/akismet.js',
     format: 'iife',
     name: 'akismet'
   }
