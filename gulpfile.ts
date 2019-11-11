fdc6fed701710f42be4972d3dfdf41de269b7eaa
Cédric Belin <cedric@belin.io>
2019-11-11 01:04:52 +0100

    Replaced `babel-minify` by `terser`for minification

--- gulpfile.ts
+++ gulpfile.ts
@@ -21,7 +21,7 @@ if (!_path.includes(_vendor)) process.env.PATH = `${_vendor}${delimiter}${_path}
 /** Builds the project. */
 task('build:dist', async () => {
   await _exec('rollup', ['--config=etc/rollup.js']);
-  return _exec('minify', ['build/free-mobile.js', '--out-file=build/free-mobile.min.js']);
+  return _exec('terser', ['--config-file=etc/terser.json', '--output=build/free-mobile.min.js', 'build/free-mobile.js']);
 });
 
 task('build:fix', () => src('lib/**/*.js').pipe(replace(/(export|import)\s+(.+)\s+from\s+'(\.[^']+)'/g, "$1 $2 from '$3.js'")).pipe(dest('lib')));
