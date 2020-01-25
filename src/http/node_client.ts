e2c707b69daf8b39ae96e57fb44e3775c2c9cf01
Cédric Belin <cedric@belin.io>
2020-01-25 21:52:30 +0100

    Fixed a bug with latest Node.js typings

--- src/http/node_client.ts
+++ src/http/node_client.ts
@@ -1,4 +1,4 @@
-import EventEmitter from 'events';
+import {EventEmitter} from 'events';
 import {packageVersion} from '../version.g';
 import {RequestEvent, ResponseEvent} from './events';
 import {ClientMixin} from './mixin';
