4d3a49558f47ebe91663604b30de3175b4f8d189
Cédric Belin <cedric@belin.io>
2020-04-10 14:11:44 +0200

    Optimized a regular expression

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -12,7 +12,7 @@ const _vendor = resolve('node_modules/.bin');
 if (!_path.includes(_vendor)) process.env.PATH = `${_vendor}${delimiter}${_path}`;
 
 /** Builds the project. */
-const esmRegex = /(export|import)\s+(.+)\s+from\s+'(\.((?!.*\.js)[^']+))'/g;
+const esmRegex = /(export|import)\s+(.+)\s+from\s+'((?!.*\.js)\.[^']+)'/g;
 task('build:fix', () => src('lib/**/*.js').pipe(replace(esmRegex, "$1 $2 from '$3.js'")).pipe(dest('lib')));
 task('build:js', () => _exec('tsc', ['--project', 'src/tsconfig.json']));
 task('build', series('build:js', 'build:fix'));
