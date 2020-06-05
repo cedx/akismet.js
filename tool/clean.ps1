98b702cd84b99e73eda70812bd8ad525026c8a13
Cédric Belin <cedric@belin.io>
2020-06-06 01:37:50 +0200

    Fixed the "clean" task

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -6,4 +6,4 @@ foreach ($item in "build", "doc/api", "lib", "www") {
 	if (Test-Path $item) { Remove-Item $item -Recurse }
 }
 
-Get-ChildItem var -Exclude .gitkeep | Remove-Item $item -Recurse
+Get-ChildItem var -Exclude .gitkeep | Remove-Item -Recurse
