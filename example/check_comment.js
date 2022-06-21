69be80e2c82f0ca32b3a69c8c8ab9c3b1cdf1f04
Cédric Belin <cedric@belin.io>
2022-06-21 19:58:35 +0200

    Added new examples

---
+++ example/check_comment.js
@@ -0,0 +1,31 @@
+import console from "node:console";
+import {Author, Blog, CheckResult, Client, Comment, CommentType} from "@cedx/akismet";
+
+/**
+ * Checks a comment against the Akismet service.
+ */
+try {
+	const author = new Author({
+		email: "john.doe@domain.com",
+		ipAddress: "192.168.123.456",
+		name: "John Doe",
+		role: "guest",
+		userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+	});
+
+	const comment = new Comment({
+		author,
+		date: new Date,
+		content: "A user comment",
+		referrer: "https://github.com/cedx/akismet.js",
+		type: CommentType.contactForm
+	});
+
+	const blog = new Blog({charset: "UTF-8", languages: ["fr"], url: "https://www.yourblog.com"});
+	const result = await new Client("123YourAPIKey", blog).checkComment(comment);
+	console.log(result == CheckResult.ham ? "The comment is ham." : "The comment is spam.");
+}
+
+catch (error) {
+	console.log(`An error occurred: ${error}`);
+}
