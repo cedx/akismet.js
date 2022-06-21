71464211d6210832cb9680b1e786683bc7651b6e
Cédric Belin <cedric@belin.io>
2022-06-21 17:48:57 +0200

    Ported the `Client` class to JS

--- test/client_test.js
+++ test/client_test.js
@@ -1,19 +1,23 @@
 import assert from "node:assert/strict";
+import {env} from "node:process";
 import test from "node:test";
-import {Author, Blog, CheckResult, Client, Comment, CommentType} from "../lib/index.js";
+import {Author, AuthorRole, Blog, CheckResult, Client, Comment, CommentType} from "../lib/index.js";
 
 // The default test client.
-const blog = new Blog("https://github.com/cedx/akismet.js");
-const _client = new Client(process.env.AKISMET_API_KEY, blog, {isTest: true});
+const client = new Client(
+	env.AKISMET_API_KEY ?? "",
+	new Blog("https://github.com/cedx/akismet.js"),
+	{isTest: true}
+);
 
 // A message marked as ham.
-let author = new Author("192.168.0.1", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0", {
+let author = new Author("192.168.0.1", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36", {
 	name: "Akismet",
-	role: "administrator",
-	url: "https://github.com/cedx/akismet.js"
+	role: AuthorRole.administrator,
+	url: "https://belin.io"
 });
 
-const _ham = new Comment(author, {
+const ham = new Comment(author, {
 	content: "I'm testing out the Service API.",
 	referrer: "https://www.npmjs.com/package/@cedx/akismet",
 	type: CommentType.comment
@@ -25,41 +29,40 @@ author = new Author("127.0.0.1", "Spam Bot/6.6.6", {
 	name: "viagra-test-123"
 });
 
-const _spam = new Comment(author, {
+const spam = new Comment(author, {
 	content: "Spam!",
-	type: CommentType.trackback
+	type: CommentType.blogPost
 });
 
 test("Client.checkComment()", async ctx => {
 	await ctx.test("should return `CheckResult.ham` for valid comment (e.g. ham)", async () => {
-		assert.equal(await _client.checkComment(_ham), CheckResult.ham);
+		assert.equal(await client.checkComment(ham), CheckResult.ham);
 	});
 
 	await ctx.test("should return `CheckResult.spam` for invalid comment (e.g. spam)", async () => {
-		const result = await _client.checkComment(_spam);
-		assert.ok(result == CheckResult.spam || result == CheckResult.pervasiveSpam);
+		const result = await client.checkComment(spam);
+		assert.ok([CheckResult.spam, CheckResult.pervasiveSpam].includes(result));
 	});
 });
 
 test("Client.submitHam()", async ctx => {
 	await ctx.test("should complete without any error", () => {
-		return assert.doesNotReject(_client.submitHam(_ham));
+		return assert.doesNotReject(client.submitHam(ham));
 	});
 });
 
 test("Client.submitSpam()", async ctx => {
 	await ctx.test("should complete without any error", () => {
-		return assert.doesNotReject(_client.submitSpam(_ham));
+		return assert.doesNotReject(client.submitSpam(spam));
 	});
 });
 
 test("Client.verifyKey()", async ctx => {
 	await ctx.test("should return `true` for a valid API key", async () => {
-		assert.ok(await _client.verifyKey());
+		assert.ok(await client.verifyKey());
 	});
 
 	await ctx.test("should return `false` for an invalid API key", async () => {
-		const client = new Client("0123456789-ABCDEF", _client.blog, {isTest: true});
-		assert.equal(await client.verifyKey(), false);
+		assert.equal(await new Client("0123456789-ABCDEF", client.blog, {isTest: true}).verifyKey(), false);
 	});
 });
