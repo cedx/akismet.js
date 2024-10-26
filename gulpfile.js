404f29a713bf2660f889d51af03532ab4c774ebe
Cédric Belin <cedric@belin.io>
2024-10-27 01:31:34 +0200

    Add a "watch" task [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,11 @@ export async function test() {
 	return $("node", ["--test", "--test-reporter=spec"]);
 }
 
+/** Watches for file changes. */
+export function watch() {
+	return $("tsc", ["--build", "src/tsconfig.json", "--preserveWatchOutput", "--sourceMap", "--watch"]);
+}
+
 /** The default task. */
 export default gulp.series(
 	clean,
