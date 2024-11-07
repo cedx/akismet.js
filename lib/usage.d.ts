0d5392e4500080854ed97b085a465cf36d2dfed8
Cédric Belin <cedric@belin.io>
2024-11-07 17:53:20 +0100

    Port the `Usage` class

--- lib/usage.d.ts
+++ lib/usage.d.ts
@@ -27,26 +27,14 @@ export class Usage {
 	 * Creates a new usage.
 	 * @param options An object providing values to initialize this instance.
 	 */
-	constructor(options: UsageOptions = {}) {
-		this.limit = options.limit ?? -1;
-		this.percentage = options.percentage ?? 0;
-		this.throttled = options.throttled ?? false;
-		this.usage = options.usage ?? 0;
-	}
+	constructor(options?: UsageOptions);
 
 	/**
 	 * Creates a new usage from the specified JSON object.
 	 * @param json A JSON object representing a usage.
 	 * @returns The instance corresponding to the specified JSON object.
 	 */
-	static fromJson(json: Record<string, any>): Usage {
-		return new this({
-			limit: Number.isInteger(json.limit) ? json.limit as number : -1,
-			percentage: typeof json.percentage == "number" ? json.percentage : 0,
-			throttled: typeof json.throttled == "boolean" ? json.throttled : false,
-			usage: Number.isInteger(json.usage) ? json.usage as number : 0
-		});
-	}
+	static fromJson(json: Record<string, any>): Usage;
 }
 
 /**
