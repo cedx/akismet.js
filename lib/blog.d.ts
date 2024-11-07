19e0476a4fdbc6cc6d696a70d9e36115fa615197
Cédric Belin <cedric@belin.io>
2024-11-07 19:24:22 +0100

    Port the `Blog` class

--- lib/blog.d.ts
+++ lib/blog.d.ts
@@ -11,7 +11,7 @@ export class Blog {
 	/**
 	 * The languages in use on the blog or site, in ISO 639-1 format.
 	 */
-	languages: Array<string>;
+	languages: Set<string>;
 
 	/**
 	 * The blog or site URL.
@@ -22,35 +22,20 @@ export class Blog {
 	 * Creates a new blog.
 	 * @param options An object providing values to initialize this instance.
 	 */
-	constructor(options: BlogOptions = {}) {
-		this.charset = options.charset ?? "";
-		this.languages = options.languages ?? [];
-		this.url = options.url ? new URL(options.url) : null;
-	}
+	constructor(options?: BlogOptions);
 
 	/**
 	 * Creates a new blog from the specified JSON object.
 	 * @param json A JSON object representing a blog.
 	 * @returns The instance corresponding to the specified JSON object.
 	 */
-	static fromJson(json: Record<string, any>): Blog {
-		return new this({
-			charset: typeof json.blog_charset == "string" ? json.blog_charset : "",
-			languages: typeof json.blog_lang == "string" ? json.blog_lang.split(",").map(language => language.trim()) : [],
-			url: typeof json.blog == "string" ? json.blog : ""
-		});
-	}
+	static fromJson(json: Record<string, any>): Blog;
 
 	/**
 	 * Returns a JSON representation of this object.
 	 * @returns The JSON representation of this object.
 	 */
-	toJSON(): Record<string, any> {
-		const map: Record<string, any> = {blog: this.url?.href ?? ""};
-		if (this.charset) map.blog_charset = this.charset;
-		if (this.languages.length) map.blog_lang = this.languages.join();
-		return map;
-	}
+	toJSON(): Record<string, any>;
 }
 
 /**
---
+++ src/blog.coffee
@@ -0,0 +1,27 @@
+# Represents the front page or home URL transmitted when making requests.
+export class Blog
+
+	# Creates a new blog.
+	constructor: (options = {}) ->
+
+		# The character encoding for the values included in comments.
+		@charset = options.charset ? ""
+
+		# The languages in use on the blog or site, in ISO 639-1 format.
+		@languages = new Set(options.languages ? [])
+
+		# The blog or site URL.
+		@url = if options.url then new URL options.url else null
+
+	# Creates a new blog from the specified JSON object.
+	@fromJson: (json) -> new @
+		charset: if typeof json.blog_charset == "string" then json.blog_charset else ""
+		languages: if typeof json.blog_lang == "string" then json.blog_lang.split(",").map (language) -> language.trim() else []
+		url: if typeof json.blog == "string" then json.blog else ""
+
+	# Returns a JSON representation of this object.
+	toJSON: ->
+		map = blog: @url?.href ? ""
+		map.blog_charset = @charset if @charset
+		map.blog_lang = Array.from(@languages).join "," if @languages.size
+		map
