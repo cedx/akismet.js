27efd26fc5cf11e9bdd0cd0246815e693bc554b3
Cédric Belin <cedric@belin.io>
2024-11-07 17:17:26 +0100

    Port the `CheckResult` enumeration

--- lib/check_result.d.ts
+++ lib/check_result.d.ts
@@ -1,7 +1,7 @@
 /**
  * Specifies the result of a comment check.
  */
-export const CheckResult = Object.freeze({
+export const CheckResult: Readonly<{
 
 	/**
 	 * The comment is not a spam (i.e. a ham).
@@ -17,7 +17,7 @@ export const CheckResult = Object.freeze({
 	 * The comment is a pervasive spam (i.e. it can be safely discarded).
 	 */
 	pervasiveSpam: 2
-});
+}>;
 
 /**
  * Specifies the result of a comment check.
