fe452f243e94e93978ff25487822ac4741a49410
Cédric Belin <cedx@outlook.com>
2025-10-06 00:52:33 +0200

    Code optimization [skip ci]

--- run.ps1
+++ run.ps1
@@ -2,4 +2,4 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
-. "$PSScriptRoot/tool/$($args.Count -eq 0 ? "Default" : $args[0]).ps1"
+. tool/$($args.Count -eq 0 ? "Default" : $args[0]).ps1
