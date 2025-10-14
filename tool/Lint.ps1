48f4a464f12f65808b82884b42973df930b3c34d
Cédric Belin <cedx@outlook.com>
2025-10-14 10:34:13 +0200

    Update the "Lint" script [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,5 +1,5 @@
 "Performing the static analysis of source code..."
-Import-Module PSScriptAnalyzer -Scope Local
+Import-Module PSScriptAnalyzer
 Invoke-ScriptAnalyzer $PSScriptRoot -Recurse
 npx tsc --build tsconfig.json --noEmit
 npx eslint --cache --cache-location=var --config=etc/ESLint.js example src test
