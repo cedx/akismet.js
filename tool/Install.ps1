d378f73f3caa300dd923b30611d235f94d008263
Cédric Belin <cedx@outlook.com>
2025-10-23 09:44:04 +0200

    Update the "Install" script [skip ci]

--- tool/Install.ps1
+++ tool/Install.ps1
@@ -1,3 +1,3 @@
 "Installing the dependencies..."
-Install-PSResource PSScriptAnalyzer -WarningAction SilentlyContinue
+Install-PSResource PSScriptAnalyzer -TrustRepository -WarningAction SilentlyContinue
 npm install
