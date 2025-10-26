556fcdedaf5e0670c0f5bfca516252acad003824
Cédric Belin <cedx@outlook.com>
2025-10-27 00:12:30 +0100

    Update the build script [skip ci]

--- invoke.ps1
+++ invoke.ps1
@@ -5,10 +5,18 @@ param (
 		param ($commandName, $parameterName, $wordToComplete)
 		(Get-Item "$PSScriptRoot/tool/$wordToComplete*.ps1").BaseName
 	})]
-	[string] $Command = "Default"
+	[string] $Command = "Default",
+
+	[Parameter()]
+	[switch] $Release
 )
 
-Set-StrictMode -Version Latest
-$ErrorActionPreference = "Stop"
-$PSNativeCommandUseErrorActionPreference = $true
-. $PSScriptRoot/tool/$Command.ps1
+begin {
+	$ErrorActionPreference = "Stop"
+	$PSNativeCommandUseErrorActionPreference = $true
+	Set-StrictMode -Version Latest
+}
+
+end {
+	. $PSScriptRoot/tool/$Command.ps1
+}
