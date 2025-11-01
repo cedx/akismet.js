4a5465648ddd7c485557ae6d79db462da3d6ab3a
Cédric Belin <cedx@outlook.com>
2025-11-01 19:01:42 +0100

    Update the build system [skip ci]

--- invoke.ps1
+++ invoke.ps1
@@ -14,4 +14,4 @@ param (
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-StrictMode -Version Latest
-. $PSScriptRoot/tool/$Command.ps1
+. "$PSScriptRoot/tool/$Command.ps1"
