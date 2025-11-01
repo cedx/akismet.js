86cf64cbdc6cb0cf746f029b5f63304ab9391401
Cédric Belin <cedx@outlook.com>
2025-11-01 22:12:56 +0100

    Update the "Publish" script [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -4,5 +4,4 @@
 $version = (Get-Content "package.json" | ConvertFrom-Json).version
 git tag "v$version"
 git push origin "v$version"
-npm publish --registry=https://registry.npmjs.org
-npm publish --registry=https://npm.pkg.github.com
+npm publish
