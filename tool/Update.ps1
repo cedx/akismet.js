5a405b27446b2386f844719dafd887895f323a83
Cédric Belin <cedx@outlook.com>
2026-01-20 19:34:58 +0100

    Code formatting [skip ci]

--- tool/Update.ps1
+++ tool/Update.ps1
@@ -1,3 +1,3 @@
 "Updating the dependencies..."
-(Import-PowerShellDataFile PSModules.psd1).Keys | ForEach-Object { Update-PSResource $_ -TrustRepository }
+Import-PowerShellDataFile PSModules.psd1 | Select-Object -ExpandProperty Keys | ForEach-Object { Update-PSResource $_ -TrustRepository }
 npm update
