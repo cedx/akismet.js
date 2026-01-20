06ce8da759a543c2a68262ca32d0e9a46d0f483e
Cédric Belin <cedx@outlook.com>
2026-01-20 19:46:52 +0100

    Code formatting [skip ci]

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,3 +1,3 @@
 "Deleting all generated files..."
-Remove-Item "lib" -ErrorAction Ignore -Force -Recurse
-Remove-Item "var/*" -Exclude ".gitkeep" -Force -Recurse
+Remove-Item lib -ErrorAction Ignore -Force -Recurse
+Remove-Item var/* -Exclude .gitkeep -Force -Recurse
--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,7 +1,7 @@
 & "$PSScriptRoot/Default.ps1"
 
 "Publishing the package..."
-$version = Get-Content "package.json" | ConvertFrom-Json | Select-Object -ExpandProperty version
+$version = Get-Content package.json | ConvertFrom-Json | Select-Object -ExpandProperty version
 git tag "v$version"
 git push origin "v$version"
 npm publish
