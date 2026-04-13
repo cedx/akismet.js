5ea33493aeb43d11969c140cfa9e9b709347bd10
Cédric Belin <cedx@outlook.com>
2026-04-13 22:46:18 +0200

    Update the dependencies [skip ci]

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,5 +1,6 @@
-"Performing the static analysis of source code..."
 Import-Module PSScriptAnalyzer
+
+"Performing the static analysis of source code..."
 Invoke-ScriptAnalyzer $PSScriptRoot -ExcludeRule PSAvoidUsingPositionalParameters -Recurse
 npx tsc --build src/tsconfig.json --noEmit
 npx eslint --cache --cache-location var --config etc/ESLint.js src test
