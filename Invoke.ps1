51e62c50680bc68b9613667f80001ee40df5a3c9
Cédric Belin <cedx@outlook.com>
2026-04-19 15:21:15 +0200

    Update the "Invoke" script [skip ci]

--- Invoke.ps1
+++ Invoke.ps1
@@ -3,9 +3,10 @@ param (
 	# The name of the task to invoke.
 	[Parameter(Position = 0)]
 	[ArgumentCompleter({
-		param ($commandName, $parameterName, $wordToComplete)
+		param ([string] $commandName, [string] $parameterName, [string] $wordToComplete)
 		(Get-Item "$PSScriptRoot/tool/$wordToComplete*.ps1").BaseName
 	})]
+	[ValidateScript({ Test-Path "$PSScriptRoot/tool/$_.ps1" -PathType Leaf }, ErrorMessage = "The specified command does not exist.")]
 	[string] $Command = "Default"
 )
 
