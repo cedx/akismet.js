da4593f591eb60d446b05db05c3cef1716149054
Cédric Belin <cedx@outlook.com>
2026-08-29 00:29:55 +0200

    Module merging [skip ci]

--- Sources/Author.ts
+++ Sources/Author.ts
@@ -71,3 +71,19 @@ export type AuthorOptions = Partial<Omit<Author, "toJSON"|"url"> & {
 	 */
 	url: URL|string;
 }>;
+
+/**
+ * Specifies the role of an author.
+ */
+export const AuthorRole = Object.freeze({
+
+	/**
+	 * The author is an administrator.
+	 */
+	Administrator: "administrator"
+});
+
+/**
+ * Specifies the role of an author.
+ */
+export type AuthorRole = typeof AuthorRole[keyof typeof AuthorRole];
--- Sources/Comment.ts
+++ Sources/Comment.ts
@@ -99,3 +99,49 @@ export type CommentOptions = Partial<Omit<Comment, "permalink"|"referrer"|"toJSO
 	 */
 	referrer: URL|string;
 }>;
+
+/**
+ * Specifies the type of a comment.
+ */
+export const CommentType = Object.freeze({
+
+	/**
+	 * A blog post.
+	 */
+	BlogPost: "blog-post",
+
+	/**
+	 * A blog comment.
+	 */
+	Comment: "comment",
+
+	/**
+	 * A contact form or feedback form submission.
+	 */
+	ContactForm: "contact-form",
+
+	/**
+	 * A top-level forum post.
+	 */
+	ForumPost: "forum-post",
+
+	/**
+	 * A message sent between just a few users.
+	 */
+	Message: "message",
+
+	/**
+	 * A reply to a top-level forum post.
+	 */
+	Reply: "reply",
+
+	/**
+	 * A new user account.
+	 */
+	Signup: "signup"
+});
+
+/**
+ * Specifies the type of a comment.
+ */
+export type CommentType = typeof CommentType[keyof typeof CommentType];
